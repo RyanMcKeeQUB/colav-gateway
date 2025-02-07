5bb19e0e0ffae28b674420d1ea1af0210031f631
Ryan <r.mckee@qub.ac.uk>
2025-02-07 15:49:48 +0000

    FIX:removing build log and install dir's accidentaly added to ws

--- build/colav_gateway/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/3507145@eeecs.qub.ac.uk/Documents/ColavProject/colav_ws/src/colav_server/colav_gateway/install/colav_gateway'
