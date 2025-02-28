629f3c59b660b7fec5e8177941fabf3658898291
Ryan McKee <r.mckee@qub.ac.uk>
2025-02-28 16:53:42 +0000

    update

--- colav_gateway/scripts/mission_interface_node.py
+++ colav_gateway/scripts/mission_interface_node.py
@@ -15,15 +15,22 @@ from colav_protobuf.missionRequest_pb2 import MissionRequest as ProtobufMissionR
 
 # ROS interface imports
 # Mission Request
-from colav_interfaces.msg import MissionRequest as RosMissionRequest # This is goal msg for service
+from colav_interfaces.msg import (
+    MissionRequest as RosMissionRequest,
+)  # This is goal msg for service
 from colav_interfaces.action import MissionExecutor
-from std_msgs.msg import ByteMultiArray # this if feedback message
+from std_msgs.msg import ByteMultiArray  # this if feedback message
 
 import numpy as np
 from geometry_msgs.msg import Point32
 from rclpy.action import ActionClient
 from rclpy.impl.rcutils_logger import RcutilsLogger
-sys.path.append(os.path.abspath('/home/3507145@eeecs.qub.ac.uk/Documents/ColavProject/colav_ws/src/colav_server/colav_gateway'))
+
+sys.path.append(
+    os.path.abspath(
+        "/home/3507145@eeecs.qub.ac.uk/Documents/ColavProject/colav_ws/src/colav_server/colav_gateway"
+    )
+)
 from typing import Any, Tuple
 from colav_gateway.utils.udp_socket_utils import setup_udp_socket
 
@@ -35,40 +42,53 @@ ACTION_NAME = "execute_colav_mission"
 
 logger = rclpy.logging.get_logger(f"{ROS_NAMESPACE}/{NODE_NAME}")
 
+
 class MissionInterfaceNode(Node):
     """ColavGateway acts as the gateway to the COLAV application. It listens to UDP sockets
     and translates received Protobuf messages into ROS messages for COLAV topics."""
 
     def __init__(
-            self, 
-            namespace:str = "colav_gateway", 
-            node_name:str = "mission_interface_node", 
-            is_thread: bool = False,
-            threading_events: dict = None
+        self,
+        namespace: str = "colav_gateway",
+        node_name: str = "mission_interface_node",
+        is_thread: bool = False,
+        threading_events: dict = None,
     ):
         """Initializes the ColavGatewayNode."""
         super().__init__(namespace=namespace, node_name=node_name)
 
-        self.declare_parameter('mission_request_host', '0.0.0.0')
-        self.declare_parameter('mission_request_port', 7000)
-        self.declare_parameter('mission_response_host', '0.0.0.0')
-        self.declare_parameter('mission_response_port', 7001)
-        self.declare_parameter('controller_feedback_host', '0.0.0.0')
-        self.declare_parameter('controller_feedback_port', 7300)
-        
+        self.declare_parameter("mission_request_host", "0.0.0.0")
+        self.declare_parameter("mission_request_port", 7000)
+        self.declare_parameter("mission_response_host", "0.0.0.0")
+        self.declare_parameter("mission_response_port", 7001)
+        self.declare_parameter("controller_feedback_host", "0.0.0.0")
+        self.declare_parameter("controller_feedback_port", 7300)
+
         # self._mission_request_address = mission_request_address
         # self._mission_response_address = mission_response_address
         self._mission_request_address = (
-            self.get_parameter('mission_request_host').get_parameter_value().string_value,
-            self.get_parameter('mission_request_port').get_parameter_value().integer_value
+            self.get_parameter("mission_request_host")
+            .get_parameter_value()
+            .string_value,
+            self.get_parameter("mission_request_port")
+            .get_parameter_value()
+            .integer_value,
         )
         self._mission_response_address = (
-            self.get_parameter('mission_response_host').get_parameter_value().string_value,
-            self.get_parameter('mission_response_port').get_parameter_value().integer_value
+            self.get_parameter("mission_response_host")
+            .get_parameter_value()
+            .string_value,
+            self.get_parameter("mission_response_port")
+            .get_parameter_value()
+            .integer_value,
         )
         self._controller_feedback_address = (
-            self.get_parameter('controller_feedback_host').get_parameter_value().string_value, 
-            self.get_parameter('controller_feedback_port').get_parameter_value().integer_value
+            self.get_parameter("controller_feedback_host")
+            .get_parameter_value()
+            .string_value,
+            self.get_parameter("controller_feedback_port")
+            .get_parameter_value()
+            .integer_value,
         )
 
         if is_thread:
@@ -78,56 +98,73 @@ class MissionInterfaceNode(Node):
 
     def listen_for_mission_request(self, action_srv_timeout: float = 5.0):
         """Starts the UDP listener for mission requests."""
-        self.get_logger().info(f"Checking /colav_gateway/execute_mission action srv is active before listening for mission request...")
+        self.get_logger().info(
+            f"Checking /colav_gateway/execute_mission action srv is active before listening for mission request..."
+        )
         # need to create an action client.
         execute_mission_cli = ActionClient(
-            self,
-            MissionExecutor,
-            '/colav_gateway/execute_mission'
+            self, MissionExecutor, "/colav_gateway/execute_mission"
         )
         if not execute_mission_cli.wait_for_server(timeout_sec=action_srv_timeout):
-            raise Exception(f"/execute_mision action srv not online therefore stopping node.")
-        
-        self.get_logger().info(f"/exeucte_mission action active, Listening for mission requests!")
+            raise Exception(
+                f"/execute_mision action srv not online therefore stopping node."
+            )
+
+        self.get_logger().info(
+            f"/exeucte_mission action active, Listening for mission requests!"
+        )
         sock = setup_udp_socket(address=self._mission_request_address, timeout=1.0)
 
         while True:
-            try: 
+            try:
                 data, client_address = sock.recvfrom(1024)
-                self.get_logger().info(f"Mission Request protobuf received from {client_address}")
+                self.get_logger().info(
+                    f"Mission Request protobuf received from {client_address}"
+                )
                 mission_request = ProtoToROSUtils.parse_mission_request(data)
-                if mission_request: 
-                    self.execute_mission(execute_mission_cli, mission_request=mission_request)
+                if mission_request:
+                    self.execute_mission(
+                        execute_mission_cli, mission_request=mission_request
+                    )
             except TimeoutError:
                 continue
             except KeyboardInterrupt:
                 self.get_logger().info("Shutting down mission listener!")
                 break
-            except Exception as e: 
-                self.get_logger().warning(e)
+            except Exception as e:
+                self.get_logger().warning(
+                    f"mission_interface_node::listen_for_mission_request {e}"
+                )
                 continue
-            
+
         sock.close()
 
-    def execute_mission(self, execute_mission_cli: ActionClient, mission_request: RosMissionRequest): 
+    def execute_mission(
+        self, execute_mission_cli: ActionClient, mission_request: RosMissionRequest
+    ):
         req = MissionExecutor.Goal()
         req.mission_request = mission_request
         send_goal_future = execute_mission_cli.send_goal_async(
-            goal=req,
-            feedback_callback=self.ctrl_feedback_callback
+            goal=req, feedback_callback=self.ctrl_feedback_callback
         )
         send_goal_future.add_done_callback(self._ctrl_goal_response_callback)
 
-    def ctrl_feedback_callback(self, feedback:MissionExecutor.Feedback, timeout:float = 5.0):
+    def ctrl_feedback_callback(
+        self, feedback: MissionExecutor.Feedback, timeout: float = 5.0
+    ):
 
-        try: 
-            self.get_logger().info(f'Feedback received: {feedback}')
-            controller_feedback = bytes(feedback.serialised_protobuf_controller_feedback.data)
+        try:
+            self.get_logger().info(f"Feedback received: {feedback}")
+            controller_feedback = bytes(
+                feedback.serialised_protobuf_controller_feedback.data
+            )
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
             sock.sendto(controller_feedback, self._controller_feedback_address)
-            self.get_logger(f'Controller feedback sent to {self._controller_feedback_address}')
-        except Exception as e: 
-            self.get_logger().error(f'{e}')
+            self.get_logger(
+                f"Controller feedback sent to {self._controller_feedback_address}"
+            )
+        except Exception as e:
+            self.get_logger().error(f"{e}")
         finally:
             sock.close()
         # Receiving feedback in byte format in feedback.serialized msg now need to send this to controller feedback socket until mission is completed status is returned!!!
@@ -138,7 +175,7 @@ class MissionInterfaceNode(Node):
         mission_response_msg = ""
         if goal_handle.accepted:
             pass
-        else: 
+        else:
             pass
         # if result:
         #     goal_status = result.status
--- colav_gateway/setup.py
+++ colav_gateway/setup.py
@@ -2,42 +2,48 @@ from setuptools import find_packages, setup
 from glob import glob
 import os
 
-package_name = 'colav_gateway'
+package_name = "colav_gateway"
 
 setup(
     name=package_name,
-    version='0.0.1',
-    packages=find_packages(exclude=['test', 'scripts', 'utils']),
+    version="0.0.1",
+    packages=find_packages(exclude=["test", "scripts", "utils"]),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', glob('launch/*.launch.py')),
-        ('share/' + package_name + '/config', glob('config/*.yml')),
-        (f'lib/python3.10/site-packages/{package_name}/scripts', glob('scripts/*.py')), # adding scripts manually so theres an explicity namespace
-        (f'lib/python3.10/site-packages/{package_name}/utils', glob('utils/*.py')), # adding utils manually so there is an explicit namespace in site-packages
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name + "/launch", glob("launch/*.launch.py")),
+        ("share/" + package_name + "/config", glob("config/*.yml")),
+        (
+            f"lib/python3.10/site-packages/{package_name}/scripts",
+            glob("scripts/*.py"),
+        ),  # adding scripts manually so theres an explicity namespace
+        (
+            f"lib/python3.10/site-packages/{package_name}/utils",
+            glob("utils/*.py"),
+        ),  # adding utils manually so there is an explicit namespace in site-packages
     ],
     install_requires=[
-        'setuptools',
-        'protobuf',
-        'numpy',
-        'PyYAML',
-        'colav-protobuf'
+        "setuptools",
+        "protobuf",
+        "numpy",
+        "PyYAML",
+        "colav-protobuf",
+        "colav-protobuf-utils",
     ],
     zip_safe=True,
-    maintainer='Ryan McKee',
-    maintainer_email='r.mckee@qub.ac.uk',
+    maintainer="Ryan McKee",
+    maintainer_email="r.mckee@qub.ac.uk",
     description=(
-        'colav_gateway is the gateway to the colav system, providing nodes for managing '
-        'a planning mission and receiving/providing feedback to external applications '
-        'utilizing COLAV for autonomous navigation.'
+        "colav_gateway is the gateway to the colav system, providing nodes for managing "
+        "a planning mission and receiving/providing feedback to external applications "
+        "utilizing COLAV for autonomous navigation."
     ),
-    license='MIT',
-    tests_require=['pytest'],
+    license="MIT",
+    tests_require=["pytest"],
     entry_points={
-        'console_scripts': [
-            'controller_interface_node = colav_gateway.execute_controller_interface_node:main',
-            'mission_interface_node = colav_gateway.execute_mission_interface_node:main'
+        "console_scripts": [
+            "controller_interface_node = colav_gateway.execute_controller_interface_node:main",
+            "mission_interface_node = colav_gateway.execute_mission_interface_node:main",
         ],
     },
 )
